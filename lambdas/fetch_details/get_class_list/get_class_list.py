1f03a825ed030b75d72131941cc1ae67226c4545
arwatunkiwala <arwa.tunkiwala@gmail.com>
2022-09-12 14:20:00 +0530

    api for get_class_list

--- lambdas/fetch_details/get_class_list/get_class_list.py
+++ lambdas/fetch_details/get_class_list/get_class_list.py
@@ -1,1 +1,64 @@
-pass
+import json
+from os import access
+from urllib import response
+import requests 
+import jwt
+import logging
+import sys
+import pandas as pd
+
+from sqlalchemy import create_engine
+from utils.constants import Const
+from utils import generate_response as api_response
+from utils import generate_traceback as api_traceback
+from utils.global_config import AuthConfig
+import get_class_list_helper as helper
+
+rds_host = AuthConfig.database['rds_host']
+user_name = AuthConfig.database['user_name']
+password = AuthConfig.database['password']
+db_name = AuthConfig.database['db_name']
+
+logger = logging.getLogger()
+logger.setLevel(10)
+
+try:
+    conn = create_engine(f'mysql+pymysql://{user_name}:{password}@{rds_host}/{db_name}')
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    sys.exit()
+
+
+def lambda_handler(event, context):
+
+    if isinstance(event.get("body"), type(None)) or not event.get("body"):
+        message = {
+            "message": f"Payload missing in the input"
+        }
+        logger.warning(message.get("message"))
+        return api_response.generate_response(status_code=400, response_body=message)
+
+    else:
+        body = json.loads(event.get("body", dict()))
+        result = helper.validate_discounts_payload(body)
+        if result != "valid":
+            logger.info("Input payload is invalid")
+            logger.debug(result)
+            return api_response.generate_response(status_code=400, response_body=result)
+    
+    class_id = body['classId']
+    query = f'select * from sys.class where id = {class_id}'
+    
+    try:
+        class_rec = pd.read_sql(query, conn)
+    except:
+        message = {"message": Const.DB_FAILURE}
+        return api_response.generate_response(status_code=500, response_body=message)
+
+    if class_rec.empty:
+        message = {"message": Const.INVALID_USER}
+        return api_response.generate_response(status_code=404, response_body=message)
+    
+    result = class_rec.to_dict('records')
+
+    return api_response.generate_response(status_code=200, response_body=result)
