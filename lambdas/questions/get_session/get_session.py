88c6b2fd62c21cf741f123eee95dff5ad8b57792
Jyotir Jain <jyotir.reply@gmail.com>
2023-01-10 13:04:26 +0530

    get sessions
    added check for already answered

--- lambdas/questions/get_session/get_session.py
+++ lambdas/questions/get_session/get_session.py
@@ -2,7 +2,7 @@ import json
 from os import access
 import secrets
 from urllib import response
-import requests 
+import requests
 import jwt
 import logging
 import sys
@@ -32,7 +32,6 @@ except:
 
 
 def lambda_handler(event, context):
-
     jwt_token = event.get("headers").get("Authorization").split('Bearer ')[1]
 
     try:
@@ -41,9 +40,9 @@ def lambda_handler(event, context):
     except Exception as e:
         message = {"message": str(e)}
         return api_response.generate_response(status_code=401, response_body=message)
-    
+
     query = f'select * from sys.users where email = "{email}"'
-    
+
     try:
         user = pd.read_sql(query, conn)
     except:
@@ -53,7 +52,7 @@ def lambda_handler(event, context):
     if user.empty:
         message = {"message": Const.INVALID_USER}
         return api_response.generate_response(status_code=404, response_body=message)
-    
+
     result = user.to_dict('records')
     user_id = result[0]['user_id']
 
@@ -73,12 +72,12 @@ def lambda_handler(event, context):
             return api_response.generate_response(status_code=400, response_body=result)
 
     secret = body['secret']
-    
+
     result = user.to_dict('records')
     if result[0]['role'] == 'teacher':
         message = {"message": Const.INVALID_USER}
         return api_response.generate_response(status_code=404, response_body=message)
-    
+
     query = f'select * from sys.students where user_id = {user_id}'
     try:
         user = pd.read_sql(query, conn)
@@ -96,14 +95,27 @@ def lambda_handler(event, context):
     except:
         message = {"message": Const.DB_FAILURE}
         return api_response.generate_response(status_code=500, response_body=message)
-    
+
     if session.empty:
-        message = {"message": Const.UNAUTHORIZED}
+        message = {"message": Const.NO_RESPONSES}
         return api_response.generate_response(status_code=404, response_body=message)
 
     session = session.to_dict('records')
     session_id = session[0]['session_id']
-    
+
+    query = f'select * from sys.responses where user_id = {user_id} and question_id in (select question_id from ' \
+            f'sys.questions where session_id = {session_id})'
+
+    try:
+        responses = pd.read_sql(query, conn)
+    except:
+        message = {"message": Const.DB_FAILURE}
+        return api_response.generate_response(status_code=500, response_body=message)
+
+    if not responses.empty:
+        message = {"message": Const.RECORD_PRESENT}
+        return api_response.generate_response(status_code=409, response_body=message)
+
     query = f'select * from sys.questions where session_id = {session_id}'
 
     try:
@@ -111,9 +123,9 @@ def lambda_handler(event, context):
     except:
         message = {"message": Const.DB_FAILURE}
         return api_response.generate_response(status_code=500, response_body=message)
-    
+
     questions = questions.to_dict('records')
-    
+
     questions = {"questions": questions}
-    
+
     return api_response.generate_response(status_code=200, response_body=questions)
